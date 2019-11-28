14c405d72bf6f7c4519c68c406f2afdcd319e4a1
Marisa Ramon <mer0522@gmail.com>
2019-11-27 18:32:20 -0600

    finish mvp with addition of create, delete functionality, model validations and nav bar

--- app/controllers/pay_accounts_controller.rb
+++ app/controllers/pay_accounts_controller.rb
@@ -8,12 +8,33 @@ class PayAccountsController < ApplicationController
     @pay_account = PayAccount.find(params[:id])
   end
 
+  def new
+    @pay_account = PayAccount.new
+  end
+
   def create
+    @pay_account = PayAccount.new(pay_account_params)
+    if @pay_account.save
+        flash[:notice] = "Account was successfully created"
+        redirect_to pay_accounts_path(@pay_account)
+    else 
+        render 'new'
+    end
   end
 
   def destroy
+    @pay_account = PayAccount.find(params[:id])
+    @pay_account.destroy
+    flash[:notice] = "Account was successfully deleted"
+    redirect_to pay_account_path
   end
 
+
+  private
+
+  def pay_account_params
+    params.require(:pay_account).permit(:nickname, :account_number, :account_type)
+  end
 
 
 end
